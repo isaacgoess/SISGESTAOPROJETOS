5e867ac838a55f22d7e529e53ca9a968cd11725e
Leonardo ribeiro <leonardo.ribeirodasvirgens@gmail.com>
2019-05-28 19:59:13 -0300

    restaurando o projeto

--- ProjectSys/MyTasks/Models/TaskModel.cs
+++ ProjectSys/MyTasks/Models/TaskModel.cs
@@ -1,15 +1,9 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MyTasks.Models
 {
     public class TaskModel
     {
-
-        public TaskModel()
-        {
-            this.relatorio = new List<relatorio>();
-        }
         public int Id { get; set; }
         [Display(Name = "Título")]
         public string Title { get; set; }
@@ -19,10 +13,5 @@ namespace MyTasks.Models
         public string Importance { get; set; }
         public string Status { get; set; }
         public int ProjectId { get; set; }
-        public virtual ICollection<relatorio> relatorio { get; set; }
-    }
-
-    public class relatorio
-    {
     }
 }
